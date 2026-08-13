f5022aaebe1812ae38dae5eb9ab65d53bf958d72
Taos Transue <spacechips@rocketmail.com>
2026-08-13 10:08:26 -0600

    add admin models

--- django_experiment_tracker/admin.py
+++ django_experiment_tracker/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from django_experiment_tracker import models
+
+
+@admin.register(models.GitCommit)
+class GitCommitAdmin(admin.ModelAdmin):
+    pass
+
+
+class ParameterEnumValueInline(admin.TabularInline):
+    model = models.ParameterEnumValue
+    min_num = 1
+    extra = 0
+
+
+@admin.register(models.ParameterEnum)
+class ParameterEnumAdmin(admin.ModelAdmin):
+    inlines = [
+        ParameterEnumValueInline,
+    ]
+
+
+class ParameterGroupInline(admin.TabularInline):
+    model = models.ParameterGroup.parameters.through
+    min_num = 0
+    extra = 0
+
+
+@admin.register(models.Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    inlines = [
+        ParameterGroupInline,
+    ]
+
+
+class ParameterInline(admin.TabularInline):
+    model = models.ParameterGroup.parameters.through
+    min_num = 0
+    extra = 0
+
+
+@admin.register(models.ParameterGroup)
+class ParameterGroupAdmin(admin.ModelAdmin):
+    exclude = ('parameters',)
+    inlines = [
+        ParameterInline,
+    ]
